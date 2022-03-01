581fd6a857e15cbccdf5a38094ae80eda3c59019
Frederick Bell <shortstop4313@gmail.com>
2022-02-28 20:54:59 -0500

    Add project files.

---
+++ LogicRundown/LogicCompares.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LogicRundown
+{
+    static class LogicCompares
+    {
+        public static bool CompareLessThan(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (InvertBoolValue((Inverter), (AlphaValue < BetaValue)));
+        }
+
+        public static bool CompareDividedBy(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (InvertBoolValue((Inverter), (BetaValue % AlphaValue == 0)));
+        }
+
+        private static bool InvertBoolValue(bool shouldInvert, bool assessment)
+        {
+            return (shouldInvert ? !assessment : assessment);
+        }
+    }
+}
